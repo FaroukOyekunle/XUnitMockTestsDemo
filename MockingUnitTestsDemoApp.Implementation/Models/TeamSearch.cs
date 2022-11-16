93890f2721ea42600952ed98dad24eaf42d0aad6
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-16 21:27:44 +0100

    Added TeamController and corresponding tests.

---
+++ MockingUnitTestsDemoApp.Implementation/Models/TeamSearch.cs
@@ -0,0 +1,20 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingUnitTestsDemoApp.Implementation.Models
+{
+    public class TeamSearch
+    {
+        [Required]
+        [Range(1, 1000)]
+        public int LeagueID { get; set; }
+        public DateTime FoundingDate { get; set; }
+        public SearchDateDirection Direction { get; set; }
+        public List<Team> Results { get; set; }
+    }
+}
