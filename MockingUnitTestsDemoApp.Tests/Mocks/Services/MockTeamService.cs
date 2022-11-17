fcd69d59a7531ee68fd2a5bb043dbd8c917a70d3
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 10:05:18 +0100

    Added TeamController and corresponding tests.

---
+++ MockingUnitTestsDemoApp.Tests/Mocks/Services/MockTeamService.cs
@@ -0,0 +1,29 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Models;
+using MockingUnitTestsDemoApp.Implementation.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingUnitTestsDemoApp.Tests.Mocks.Services
+{
+    public class MockTeamService : Mock<ITeamService>
+    {
+        public MockTeamService MockSearch(List<Team> results)
+        {
+            Setup(x => x.Search(It.IsAny<TeamSearch>()))
+                .Returns(results);
+
+            return this;
+        }
+
+        public MockTeamService VerifySearch(Times times)
+        {
+            Verify(x => x.Search(It.IsAny<TeamSearch>()), times);
+
+            return this;
+        }
+    }
+}
