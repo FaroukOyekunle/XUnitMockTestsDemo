6148b6d03918a160720aa169ab2b022c3d7a0aaf
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 10:06:33 +0100

    Removed Async code, added League Controller, Player Controller, and Home

---
+++ MockingUnitTestsDemoApp.Tests/Mocks/Services/MockPlayerService.cs
@@ -0,0 +1,52 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Models;
+using MockingUnitTestsDemoApp.Implementation.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingUnitTestsDemoApp.Tests.Mocks.Services
+{
+    public class MockPlayerService : Mock<IPlayerService>
+    {
+        public MockPlayerService MockGetByID(Player result)
+        {
+            Setup(x => x.GetByID(It.IsAny<int>()))
+                .Returns(result);
+
+            return this;
+        }
+
+        public MockPlayerService MockGetByIDInvalid()
+        {
+            Setup(x => x.GetByID(It.IsAny<int>()))
+                .Throws(new Exception("Player with that ID was not found!"));
+
+            return this;
+        }
+
+        public MockPlayerService VerifyGetByID(Times times)
+        {
+            Verify(x => x.GetByID(It.IsAny<int>()), times);
+
+            return this;
+        }
+
+        public MockPlayerService MockGetForLeague(List<Player> results)
+        {
+            Setup(x => x.GetForLeague(It.IsAny<int>()))
+                .Returns(results);
+
+            return this;
+        }
+
+        public MockPlayerService VerifyGetForLeague(Times times)
+        {
+            Verify(x => x.GetForLeague(It.IsAny<int>()), times);
+
+            return this;
+        }
+    }
+}
