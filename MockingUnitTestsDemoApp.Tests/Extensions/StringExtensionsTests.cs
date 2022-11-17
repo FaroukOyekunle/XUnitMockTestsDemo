73e6dcfdcd5d6fe196ffd11d818c9d23f760e307
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 10:09:38 +0100

    Added a couple of tests for CIEquals.

---
+++ MockingUnitTestsDemoApp.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockingUnitTestsDemoApp.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("Test Case", "test case")]
+        [InlineData("TeSt CaSe", "tEsT cAsE")]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        public void StringExtensions_CIEquals_TrueCases(string first, string second)
+        {
+            var result = first.CIEquals(second);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("Test Case", "test case ")]
+        [InlineData("TeSt CaSe", "tEsT cAsE 2")]
+        [InlineData("Test Case", null)]
+        [InlineData("", "Test Case")]
+        public void StringExtensions_CIEquals_FalseCases(string first, string second)
+        {
+            var result = first.CIEquals(second);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("", null)]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        public void StringExtensions_CIEquals_NullEqualsEmpty_TrueCases(string first, string second)
+        {
+            var result = first.CIEquals(second, CIEqualsOption.NullEqualsEmpty);
+            Assert.True(result);
+        }
+    }
+}
