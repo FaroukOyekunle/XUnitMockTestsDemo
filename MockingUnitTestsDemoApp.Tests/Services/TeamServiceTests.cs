389bb9868fa3efd0b07a946b0cf8e6d20982fc81
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 10:00:37 +0100

    Minor naming changes

---
+++ MockingUnitTestsDemoApp.Tests/Services/TeamServiceTests.cs
@@ -0,0 +1,164 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Enums;
+using MockingUnitTestsDemoApp.Implementation.Models;
+using MockingUnitTestsDemoApp.Implementation.Services;
+using MockingUnitTestsDemoApp.Tests.Mocks.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockingUnitTestsDemoApp.Tests.Services
+{
+    public class TeamServiceTests
+    {
+        [Fact]
+        public void TeamService_Search_OlderThan_Valid()
+        {
+            var mockTeams = GetMockTeams();
+
+            var mockTeamRepo = new MockTeamRepository().MockGetForLeague(mockTeams);
+            var mockLeagueRepo = new MockLeagueRepository().MockIsValid(true);
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockLeagueRepo.Object);
+
+            var searchParams = new TeamSearch()
+            {
+                LeagueID = 1,
+                FoundingDate = new DateTime(2013, 1, 1),
+                Direction = SearchDateDirection.OlderThan
+            };
+
+            //Act
+            var results = teamService.Search(searchParams);
+
+            Assert.NotEmpty(results);
+            mockLeagueRepo.VerifyIsValid(Times.Once());
+            mockTeamRepo.VerifyGetForLeague(Times.Once());
+        }
+
+        [Fact]
+        public void TeamService_Search_NewerThan_Valid()
+        {
+            var mockTeams = GetMockTeams();
+
+            var mockTeamRepo = new MockTeamRepository().MockGetForLeague(mockTeams);
+            var mockLeagueRepo = new MockLeagueRepository().MockIsValid(true);
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockLeagueRepo.Object);
+
+            var searchParams = new TeamSearch()
+            {
+                LeagueID = 1,
+                FoundingDate = new DateTime(1969, 1, 1),
+                Direction = SearchDateDirection.NewerThan
+            };
+
+            //Act
+            var results = teamService.Search(searchParams);
+
+            Assert.NotEmpty(results);
+            mockLeagueRepo.VerifyIsValid(Times.Once());
+            mockTeamRepo.VerifyGetForLeague(Times.Once());
+        }
+
+        [Fact]
+        public void TeamService_Search_OlderThan_Invalid()
+        {
+            var mockTeams = GetMockTeams();
+
+            var mockTeamRepo = new MockTeamRepository().MockGetForLeague(mockTeams);
+            var mockLeagueRepo = new MockLeagueRepository().MockIsValid(true);
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockLeagueRepo.Object);
+
+            var searchParams = new TeamSearch()
+            {
+                LeagueID = 1,
+                FoundingDate = new DateTime(1966, 1, 1),
+                Direction = SearchDateDirection.OlderThan
+            };
+
+            //Act
+            var results = teamService.Search(searchParams);
+
+            Assert.Empty(results);
+            mockLeagueRepo.VerifyIsValid(Times.Once());
+            mockTeamRepo.VerifyGetForLeague(Times.Once());
+        }
+
+        [Fact]
+        public void TeamService_Search_NewerThan_Invalid()
+        {
+            var mockTeams = GetMockTeams();
+
+            var mockTeamRepo = new MockTeamRepository().MockGetForLeague(mockTeams);
+            var mockLeagueRepo = new MockLeagueRepository().MockIsValid(true);
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockLeagueRepo.Object);
+
+            var searchParams = new TeamSearch()
+            {
+                LeagueID = 1,
+                FoundingDate = new DateTime(2017, 1, 1),
+                Direction = SearchDateDirection.NewerThan
+            };
+
+            //Act
+            var results = teamService.Search(searchParams);
+
+            Assert.Empty(results);
+            mockLeagueRepo.VerifyIsValid(Times.Once());
+            mockTeamRepo.VerifyGetForLeague(Times.Once());
+        }
+
+        [Fact]
+        public void TeamService_Search_InvalidLeague()
+        {
+            var mockTeams = GetMockTeams();
+
+            var mockTeamRepo = new MockTeamRepository().MockGetForLeague(mockTeams);
+            var mockLeagueRepo = new MockLeagueRepository().MockIsValid(false);
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockLeagueRepo.Object);
+
+            var searchParams = new TeamSearch()
+            {
+                LeagueID = 1,
+                FoundingDate = new DateTime(1997, 1, 1),
+                Direction = SearchDateDirection.NewerThan
+            };
+
+            //Act
+            var results = teamService.Search(searchParams);
+
+            Assert.Empty(results);
+            mockLeagueRepo.VerifyIsValid(Times.Once());
+            mockTeamRepo.VerifyGetForLeague(Times.Never());
+        }
+
+        private List<Team> GetMockTeams()
+        {
+            return new List<Team>()
+            {
+                new Team()
+                {
+                    ID = 1,
+                    FoundingDate = new DateTime(1970, 1, 1)
+                },
+                new Team()
+                {
+                    ID = 2,
+                    FoundingDate = new DateTime(1994, 12, 1)
+                },
+                new Team()
+                {
+                    ID = 3,
+                    FoundingDate = new DateTime(2012, 5, 12)
+                }
+            };
+        }
+    }
+}
