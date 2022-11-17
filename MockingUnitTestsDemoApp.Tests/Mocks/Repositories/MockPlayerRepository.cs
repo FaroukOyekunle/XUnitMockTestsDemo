d0a63354fd4439b067739d79b35cef88c465d9fb
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 10:03:19 +0100

    Removed Async code, added League Controller, Player Controller, and Home

---
+++ MockingUnitTestsDemoApp.Tests/Mocks/Repositories/MockPlayerRepository.cs
@@ -0,0 +1,37 @@
+﻿using MockingUnitTestsDemoApp.Implementation.Models;
+using MockingUnitTestsDemoApp.Implementation.Repositories.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingUnitTestsDemoApp.Tests.Mocks.Repositories
+{
+    public class MockPlayerRepository : Mock<IPlayerRepository>
+    {
+        public MockPlayerRepository MockGetByID(Player result)
+        {
+            Setup(x => x.GetByID(It.IsAny<int>()))
+                .Returns(result);
+
+            return this;
+        }
+
+        public MockPlayerRepository MockGetForTeam(List<Player> results)
+        {
+            Setup(x => x.GetForTeam(It.IsAny<int>()))
+                .Returns(results);
+
+            return this;
+        }
+
+        public MockPlayerRepository VerifyGetForTeam(Times times)
+        {
+            Verify(x => x.GetForTeam(It.IsAny<int>()), times);
+
+            return this;
+        }
+    }
+}
