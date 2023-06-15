f0f23e1a3f42ddec0c3e3e28fec2effdf1723d96
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-15 22:22:03 +0100

    Added HomeController with action methods and error handling

--- MockingUnitTestsDemoApp/Controllers/HomeController.cs
+++ MockingUnitTestsDemoApp/Controllers/HomeController.cs
@@ -2,30 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using MockingUnitTestsDemoApp.Models;
 
-namespace MockingUnitTestsDemoApp.Controllers;
-
-public class HomeController : Controller
+namespace MockingUnitTestsDemoApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        // Action method for the Index page
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // Action method for the Privacy page
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Action method for handling errors
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            // Generate an error view with the request ID or trace identifier
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
