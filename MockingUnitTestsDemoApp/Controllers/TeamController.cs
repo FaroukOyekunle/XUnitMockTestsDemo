aeadc2df09b6607b31115b8ec0ee321497300a24
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-16 10:52:37 +0100

    Added TeamController with Search (GET/POST) actions.

--- MockingUnitTestsDemoApp/Controllers/TeamController.cs
+++ MockingUnitTestsDemoApp/Controllers/TeamController.cs
@@ -20,30 +20,38 @@ namespace MockingUnitTestsDemoApp.Controllers
         [HttpGet]
         public IActionResult Search()
         {
+            // Create a new instance of TeamSearch model
             var teamSearch = new TeamSearch();
 
+            // Return the view with the empty teamSearch model
             return View(teamSearch);
         }
 
         [HttpPost]
         public IActionResult Search(TeamSearch search)
         {
-            if(!ModelState.IsValid)
+            // Check if the model state is valid
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction("Search"); //Assume there is some manner of implementing PRG pattern here.
+                // Redirect to the Search action (GET) if the model state is not valid
+                return RedirectToAction("Search"); // Assume there is some manner of implementing the PRG Pattern
             }
 
+            // Perform the search using the TeamService and the provided search criteria
             var results = _teamService.Search(search);
 
-            if(!results.Any())
+            // Check if there are no search results
+            if (!results.Any())
             {
+                // Redirect to the Search action (GET) if there are no results
                 return RedirectToAction("Search");
             }
-
             else
             {
+                // Set the search results in the search model
                 search.Results = results;
 
+                // Return the view with the populated search model
                 return View(search);
             }
         }
