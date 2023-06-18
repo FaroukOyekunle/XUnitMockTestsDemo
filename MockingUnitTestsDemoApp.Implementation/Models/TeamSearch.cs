6f07de564cc71ee43df0380bc19f2e8624fefece
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-18 13:23:26 +0100

    Added TeamSearch class to represent a team search with league ID, founding date, direction, and results properties.

--- MockingUnitTestsDemoApp.Implementation/Models/TeamSearch.cs
+++ MockingUnitTestsDemoApp.Implementation/Models/TeamSearch.cs
@@ -1,20 +1,27 @@
-﻿using MockingUnitTestsDemoApp.Implementation.Enums;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MockingUnitTestsDemoApp.Implementation.Enums; 
+using System; 
+using System.Collections.Generic; 
+using System.ComponentModel.DataAnnotations; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
 
 namespace MockingUnitTestsDemoApp.Implementation.Models
 {
     public class TeamSearch
     {
+        // // Property representing the ID of the league to search for teams
         [Required]
         [Range(1, 1000)]
-        public int LeagueID { get; set; }
-        public DateTime FoundingDate { get; set; }
+        public int LeagueID { get; set; } 
+
+        // Property representing the founding date to filter teams
+        public DateTime FoundingDate { get; set; } 
+
+        // Property representing the direction of the search (e.g., ascending or descending)
         public SearchDateDirection Direction { get; set; }
-        public List<Team> Results { get; set; }
+
+        // Property representing the list of teams that match the search criteria
+        public List<Team> Results { get; set; } 
     }
 }
