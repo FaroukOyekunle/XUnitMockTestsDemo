ea2a0dfcbaea0013b5925d264ea2364eac19a17a
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-18 13:17:30 +0100

    Added League class to represent a league with ID, name, and founding date properties.

--- MockingUnitTestsDemoApp.Implementation/Models/League.cs
+++ MockingUnitTestsDemoApp.Implementation/Models/League.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace MockingUnitTestsDemoApp.Implementation.Models
 {
     public class League
     {
-        public int ID { get; set; }
+        // Property representing the ID of the league
+        public int ID { get; set; } 
+
+        // Property representing the name of the league
         public string Name { get; set; }
-        public DateTime FoundingDate { get; set; }
+
+        // Property representing the founding date of the league 
+        public DateTime FoundingDate { get; set; } 
     }
 }
