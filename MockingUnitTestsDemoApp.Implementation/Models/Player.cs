83bbd1b8f9999fc173d420e415b101148ffc6918
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-18 13:19:47 +0100

    Added Player class to represent a player with ID, first name, last name, date of birth, and team ID properties.

--- MockingUnitTestsDemoApp.Implementation/Models/Player.cs
+++ MockingUnitTestsDemoApp.Implementation/Models/Player.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
 
 namespace MockingUnitTestsDemoApp.Implementation.Models
 {
     public class Player
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public int TeamID { get; set; }
+        // Property representing the ID of the player
+        public int ID { get; set; } 
+
+        // Property representing the first name of the player
+        public string FirstName { get; set; } 
+
+        // Property representing the last name of the player
+        public string LastName { get; set; } 
+
+        // Property representing the date of birth of the player
+        public DateTime DateOfBirth { get; set; } 
+
+        // Property representing the ID of the team the player belongs to
+        public int TeamID { get; set; } 
     }
 }
