78ef37398aa55cb978111ec0f3f02446a1533a4e
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-20 15:34:04 +0100

    Added IPlayerRepository interface to define the contract for Player repositories, specifying methods to get a player by ID and get all players for a team.

--- MockingUnitTestsDemoApp.Implementation/Repositories/Interfaces/IPlayerRepository.cs
+++ MockingUnitTestsDemoApp.Implementation/Repositories/Interfaces/IPlayerRepository.cs
@@ -1,4 +1,7 @@
-﻿using MockingUnitTestsDemoApp.Implementation.Models;
+﻿// Added IPlayerRepository interface to define the contract for Player repositories
+// Specifies the methods to get a player by ID and get all players for a team
+
+using MockingUnitTestsDemoApp.Implementation.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,10 @@ namespace MockingUnitTestsDemoApp.Implementation.Repositories.Interfaces
 {
     public interface IPlayerRepository
     {
+        // Method to get a player by its ID
         Player GetByID(int id);
+        
+        // Method to get all players for a team by team ID
         List<Player> GetForTeam(int id);
     }
 }
