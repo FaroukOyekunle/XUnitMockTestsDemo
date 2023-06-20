a3d557efbdc549f41fe8a64c2b1c1f641c24f4bf
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-20 15:37:07 +0100

    Added ITeamRepository interface to define the contract for Team repositories, specifying methods to get a team by ID and get all teams for a league.

--- MockingUnitTestsDemoApp.Implementation/Repositories/Interfaces/ITeamRepository.cs
+++ MockingUnitTestsDemoApp.Implementation/Repositories/Interfaces/ITeamRepository.cs
@@ -1,4 +1,7 @@
-﻿using MockingUnitTestsDemoApp.Implementation.Models;
+﻿// Added ITeamRepository interface to define the contract for Team repositories
+// Specifies the methods to get a team by ID and get all teams for a league
+
+using MockingUnitTestsDemoApp.Implementation.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,10 @@ namespace MockingUnitTestsDemoApp.Implementation.Repositories.Interfaces
 {
     public interface ITeamRepository
     {
+        // Method to get a team by its ID
         Team GetByID(int id);
+        
+        // Method to get all teams for a league by league ID
         List<Team> GetForLeague(int leagueID);
     }
 }
