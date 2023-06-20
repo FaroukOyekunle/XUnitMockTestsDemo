e1c4dfeef4cbe5b859ae6caaf112c9cb1f69dd43
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-20 15:39:14 +0100

    Added LeagueRepository class that implements the ILeagueRepository interface with placeholder implementations for the interface methods.

--- MockingUnitTestsDemoApp.Implementation/Repositories/LeagueRepository.cs
+++ MockingUnitTestsDemoApp.Implementation/Repositories/LeagueRepository.cs
@@ -1,4 +1,7 @@
-﻿using MockingUnitTestsDemoApp.Implementation.Models;
+﻿// Added LeagueRepository class that implements the ILeagueRepository interface
+// Contains placeholder implementations for the interface methods
+
+using MockingUnitTestsDemoApp.Implementation.Models;
 using MockingUnitTestsDemoApp.Implementation.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,16 +13,19 @@ namespace MockingUnitTestsDemoApp.Implementation.Repositories
 {
     public class LeagueRepository : ILeagueRepository
     {
+        // Placeholder implementation for GetByID method
         public League GetByID(int leagueID)
         {
             throw new NotImplementedException();
         }
 
+        // Placeholder implementation for GetAll method
         public List<League> GetAll()
         {
             throw new NotImplementedException();
         }
 
+        // Placeholder implementation for IsValid method
         public bool IsValid(int leagueID)
         {
             throw new NotImplementedException();
