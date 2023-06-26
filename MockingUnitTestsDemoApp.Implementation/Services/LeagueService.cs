ca22a406c72e7d75d7f922b77e562ab6422119d7
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-26 10:46:37 +0100

    Implemented LeagueService class for managing leagues

--- MockingUnitTestsDemoApp.Implementation/Services/LeagueService.cs
+++ MockingUnitTestsDemoApp.Implementation/Services/LeagueService.cs
@@ -9,23 +9,27 @@ using System.Threading.Tasks;
 
 namespace MockingUnitTestsDemoApp.Implementation.Services
 {
+    // Represents a service for managing leagues
     public class LeagueService : ILeagueService
     {
-        private readonly ILeagueRepository _leagueRepository;
+        private readonly ILeagueRepository _leagueRepository; // Repository for league data
 
+        // Constructor that takes an ILeagueRepository instance as a parameter
         public LeagueService(ILeagueRepository leagueRepository)
         {
-            _leagueRepository = leagueRepository;
+            _leagueRepository = leagueRepository; // Assign the repository instance to the private field
         }
 
+        // Checks if a league with the specified ID is valid
         public bool IsValid(int id)
         {
-            return _leagueRepository.IsValid(id);
+            return _leagueRepository.IsValid(id); // Delegate the validation to the repository
         }
 
+        // Retrieves a list of all leagues
         public List<League> GetAll()
         {
-            return _leagueRepository.GetAll();
+            return _leagueRepository.GetAll(); // Retrieve all leagues from the repository
         }
     }
 }
