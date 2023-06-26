12d635665c8e8082cbf1b355416ff74b05c6f36e
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-06-26 10:48:40 +0100

    Implemented PlayerService class for managing players

--- MockingUnitTestsDemoApp.Implementation/Services/PlayerService.cs
+++ MockingUnitTestsDemoApp.Implementation/Services/PlayerService.cs
@@ -9,44 +9,49 @@ using System.Threading.Tasks;
 
 namespace MockingUnitTestsDemoApp.Implementation.Services
 {
+    // Represents a service for managing players
     public class PlayerService : IPlayerService
     {
-        private readonly IPlayerRepository _playerRepository;
-        private readonly ITeamRepository _teamRepository;
-        private readonly ILeagueRepository _leagueRepository;
+        private readonly IPlayerRepository _playerRepository; // Repository for player data
+        private readonly ITeamRepository _teamRepository; // Repository for team data
+        private readonly ILeagueRepository _leagueRepository; // Repository for league data
 
+        // Constructor that takes three repository instances as parameters
         public PlayerService(IPlayerRepository playerRepository,
-                                ITeamRepository teamRepository,
-                                ILeagueRepository leagueRepository)
+                             ITeamRepository teamRepository,
+                             ILeagueRepository leagueRepository)
         {
-            _playerRepository = playerRepository;
-            _teamRepository = teamRepository;
-            _leagueRepository = leagueRepository;
+            _playerRepository = playerRepository; // Assign the player repository instance
+            _teamRepository = teamRepository; // Assign the team repository instance
+            _leagueRepository = leagueRepository; // Assign the league repository instance
         }
 
+        // Retrieves a player by ID
         public Player GetByID(int id)
         {
-            return _playerRepository.GetByID(id);
+            return _playerRepository.GetByID(id); // Delegate the retrieval to the player repository
         }
 
+        // Retrieves all players for a given league
         public List<Player> GetForLeague(int leagueID)
         {
-            var isValidLeague = _leagueRepository.IsValid(leagueID);
+            var isValidLeague = _leagueRepository.IsValid(leagueID); // Check if the league is valid
+
             if (!isValidLeague)
             {
-                return new List<Player>();
-            }   
+                return new List<Player>(); // Return an empty list if the league is invalid
+            }
 
             List<Player> players = new List<Player>();
 
-            var teams = _teamRepository.GetForLeague(leagueID);
+            var teams = _teamRepository.GetForLeague(leagueID); // Retrieve all teams for the league
 
             foreach (var team in teams)
             {
-                players.AddRange(_playerRepository.GetForTeam(team.ID));
+                players.AddRange(_playerRepository.GetForTeam(team.ID)); // Retrieve players for each team
             }
 
-            return players;
+            return players; // Return the list of players
         }
     }
 }
